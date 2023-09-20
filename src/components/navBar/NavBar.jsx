7f0f0bacd004ac79c751b9112e9abc4958440398
AnnaGolitsyna <110576gav@gmail.com>
2023-09-20 11:49:29 +0300

    menuNavigation

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,83 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Layout } from 'antd';
-import { Link } from 'react-router-dom';
-import {
-  ShoppingCartOutlined,
-  ProjectOutlined,
-  UsergroupAddOutlined,
-  ImportOutlined,
-  BarcodeOutlined,
-  DollarOutlined,
-  HomeFilled,
-} from '@ant-design/icons';
 import { Menu } from 'antd';
-
-
-function getItem(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  };
-}
-const items = [
-  getItem(<Link to="/">На главную</Link>, 'home', <HomeFilled />),
-  getItem('Продажи', 'sub1', <ShoppingCartOutlined />, [
-    getItem(<Link to="clients/invoices">Реестр</Link>, '1'),
-
-    getItem(
-      'Создать новую',
-      'g2',
-      null,
-      [getItem('Накладную', '2'), getItem('Сверку', '3')],
-      'group'
-    ),
-  ]),
-  getItem('Поставки', 'sub2', <ImportOutlined />, [
-    getItem(<Link to="suppliers/invoices">Реестр</Link>, '4'),
-    getItem(
-      'Создать новую',
-      'g2',
-      null,
-      [getItem('Накладную', '5'), getItem('Сверку', '6')],
-      'group'
-    ),
-  ]),
-
-  getItem('Касса', '7', <DollarOutlined />),
-  getItem('Отчеты', 'sub3', <ProjectOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
-  ]),
-  getItem(
-    'Справочники',
-    'grp',
-    null,
-    [
-      getItem('Клиенты', '13', <UsergroupAddOutlined />),
-      getItem('Товары', '14', <BarcodeOutlined />),
-    ],
-    'group'
-  ),
-];
+import { items } from './menuItems.js';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   const onClick = (e) => {
     console.log('click ', e.key);
+    navigate(e.key);
   };
+  
   return (
     <Layout.Sider>
       <Menu
         onClick={onClick}
         style={{
-          // width: '100%',
           backgroundColor: 'transparent',
         }}
-        // defaultSelectedKeys={['1']}
+        defaultSelectedKeys={['home']}
         defaultOpenKeys={['sub1']}
         mode="inline"
         items={items}
@@ -87,40 +29,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-/* <Space direction="vertical" size="small" style={{ padding: '20px' }}>
-        <Divider>Клиенты</Divider>
-        <Link to="clients/get">
-          <PlusSquareFilled style={{ marginRight: '10px' }} />
-          <span>Расходные накладные</span>
-        </Link>
-        <Link to="*">
-          <MinusSquareFilled style={{ marginRight: '10px' }} />
-          <span>Возврат на склад</span>
-        </Link>
-
-        <Divider>Поставщики</Divider>
-        <Link to="*">
-          <PlusSquareFilled style={{ marginRight: '10px' }} />
-          <span>Приходные накладные</span>
-        </Link>
-        <Link to="*">
-          <MinusSquareFilled style={{ marginRight: '10px' }} />
-          <span>Возврат поставщику</span>
-        </Link>
-
-        <Divider>Касса</Divider>
-        <Link to="*">
-          <PlusSquareFilled style={{ marginRight: '10px' }} />
-          <span>Приход в кассу</span>
-        </Link>
-        <Link to="*">
-          <MinusSquareFilled style={{ marginRight: '10px' }} />
-          <span>Расход из кассы</span>
-        </Link>
-
-        <Divider>Отчеты</Divider>
-        <Link to="*">Сверка с контрагентом</Link>
-        <Link to="*">Сводный отчет по всем контрагентам</Link>
-        <Link to="*">Список товаров</Link>
-      </Space> */
---
+++ src/components/navBar/menuItems.js
@@ -0,0 +1,64 @@
+import {
+  ShoppingCartOutlined,
+  ProjectOutlined,
+  UsergroupAddOutlined,
+  ImportOutlined,
+  BarcodeOutlined,
+  DollarOutlined,
+  HomeFilled,
+} from '@ant-design/icons';
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('На главную', '/', <HomeFilled />),
+  getItem('Продажи', 'sub1', <ShoppingCartOutlined />, [
+    getItem('Реестр', 'clients/invoices'),
+
+    getItem(
+      'Создать новую',
+      'g1',
+      null,
+      [getItem('Накладную', '2'), getItem('Сверку', '3')],
+      'group'
+    ),
+  ]),
+  getItem('Поставки', 'sub2', <ImportOutlined />, [
+    getItem('Реестр', 'suppliers/invoices'),
+    getItem(
+      'Создать новую',
+      'g2',
+      null,
+      [getItem('Накладную', '5'), getItem('Сверку', '6')],
+      'group'
+    ),
+  ]),
+
+  getItem('Касса', '7', <DollarOutlined />),
+  getItem('Отчеты', 'sub3', <ProjectOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+  getItem(
+    'Справочники',
+    'grp',
+    null,
+    [
+      getItem('Клиенты', '13', <UsergroupAddOutlined />),
+      getItem('Товары', '14', <BarcodeOutlined />),
+    ],
+    'group'
+  ),
+];
+
+export { items };
