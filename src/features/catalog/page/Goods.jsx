6cff392083f1b32eebfa91b0abde06e7cd1eed92
AnnaGolitsyna <110576gav@gmail.com>
2023-11-11 13:07:23 +0200

    formArr

--- src/features/catalog/page/Goods.jsx
+++ src/features/catalog/page/Goods.jsx
@@ -1,44 +1,218 @@
 import React from 'react';
 //import PropTypes from 'prop-types'
-import { DatePicker, Form } from 'antd';
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-dayjs.extend(customParseFormat);
-const dateFormat = 'YYYY/MM/DD';
+import {
+  Form,
+  Input,
+  DatePicker,
+  Space,
+  Typography,
+  Checkbox,
+  Select,
+  theme,
+} from 'antd';
+import { IdcardTwoTone } from '@ant-design/icons';
+//import CategoryFormItem from './CategoryFormItem';
+import { emptyContractorObject } from '../utils/emptyContractorForm';
+import { categoryContractor } from '../utils/categoryContractor';
 
-const Goods = (props) => {
-  const dataTest = [
+const { useToken } = theme;
+
+const Goods = () => {
+  const [form] = Form.useForm();
+  const { token } = useToken();
+
+  const handleCategoryChange = (value) => {
+    form.setFieldsValue({ categoryPrice: undefined });
+  };
+
+  const contractorsFormList = [
     {
-      contractDate: '2023-10-07T08:32:14.541Z',
+      name: 'name',
+      label: 'Наименование',
+      component: <Input placeholder="сокращенное имя компании" />,
+      rules: [{ required: true, message: 'Заполните обязательное поле' }],
+      hasFeedback: true,
+    },
+    {
+      name: 'fullName',
+      label: 'Полное наименование',
+      component: (
+        <Input.TextArea
+          placeholder="полное наиенование компании (для документов)"
+          rows={2}
+        />
+      ),
+      rules: [{ required: true, message: 'Заполните обязательное поле' }],
+      hasFeedback: true,
+    },
+    {
+      name: 'category',
+      label: 'Категория контрагента',
+      hasFeedback: true,
+      rules: [{ required: true, message: 'Выберите категорию из списка' }],
+      component: (
+        <Select
+          placeholder="выбери категорию"
+          options={categoryContractor}
+          onChange={handleCategoryChange}
+        />
+      ),
+    },
+    {
+      name: 'categoryPrice',
+      label: 'Категория цен',
+      hasFeedback: true,
+      rules: [
+        {
+          required: true,
+          message: 'Выберите категорию из списка',
+        },
+      ],
+      condition: (prevValues, currentValues) =>
+        prevValues.category !== currentValues.category,
+
+      component: (optionsPrices) => (
+        <Select placeholder="выбери категорию цен" options={optionsPrices} />
+      ),
+    },
+
+    {
+      label: 'E-mail',
+      name: 'email',
+      rules: [{ type: 'email' }],
+      component: <Input placeholder="e-mail" />,
+    },
+    {
+      label: 'Tелефон',
+      name: 'phone',
+      component: <Input placeholder="номер телефона" />,
+    },
+    {
+      label: 'Код ОКППО/ИНН',
+      name: 'taxNumber',
+      tooltip: 'Налоговый код',
+      rules: [
+        {
+          type: 'number',
+          message: 'Введите только числа',
+          validator: (rule, value) => {
+            if (!value || /^[0-9]+$/.test(value)) {
+              return Promise.resolve();
+            }
+            return Promise.reject('Введите только числа');
+          },
+        },
+      ],
+      component: <Input placeholder="налоговый код" />,
+    },
+    {
+      label: 'Договор №',
+      name: 'contractNumber',
+      component: <Input placeholder="номер договора" />,
+    },
+    {
+      label: 'от',
+      name: 'date',
+      component: <DatePicker placeholder="дата" format="YYYY-MM-DD" />,
+    },
+    {
+      label: 'Адрес',
+      name: 'adress',
+      component: (
+        <Input.TextArea placeholder="полный адрес (для документов)" rows={3} />
+      ),
+    },
+    {
+      name: 'active',
+      valuePropName: 'checked',
+      component: <Checkbox>Активный контрагент</Checkbox>,
     },
   ];
 
-  const [form] = Form.useForm();
-  const testDate = { date: dayjs(dataTest[0].contractDate) };
-
-  console.log('goods', dayjs(dataTest[0].contractDate, dateFormat));
-  console.log('goodsForm', form, testDate.date, form.getFieldValue('date'));
-
-console.log('testDate', testDate);
-
   return (
-    <div>
-      Goods
-      <DatePicker
-        defaultValue={dayjs(dataTest.contractDate, dateFormat)}
-        format={dateFormat}
-      />
-      <Form form={form} initialValues={testDate}>
-        <Form.Item name={'date'}>
-          <DatePicker  />
+    <Form
+      name="contractor"
+      layout="vertical"
+      form={form}
+      initialValues={emptyContractorObject}
+    >
+      <Space.Compact
+        block
+        style={{ alignItems: 'flex-start', justifyContent: 'space-evenly' }}
+      >
+        <IdcardTwoTone
+          twoToneColor={token.colorInfo}
+          style={{ fontSize: 40 }}
+        />
+        <Typography.Title level={3} style={{ marginTop: 0 }}>
+          Новый контрагент
+        </Typography.Title>
+        <Form.Item name={'id'} style={{ display: 'none' }}>
+          <Input type="hidden" />
         </Form.Item>
-      </Form>
-    </div>
+      </Space.Compact>
+
+      {contractorsFormList.map((element) => {
+        if (element.condition) {
+          console.log('if', element, element.component);
+          return (
+            <Form.Item
+              key={element.name}
+              noStyle
+              shouldUpdate={(prevValues, currentValues) =>
+                prevValues.category !== currentValues.category
+              }
+            >
+              {({ getFieldValue }) => {
+                const categoryDetails = categoryContractor.find(
+                  (category) => category.value === getFieldValue('category')
+                );
+
+                const optionsPrices = categoryDetails?.children?.map(
+                  ({ label, value }) => ({
+                    label,
+                    value,
+                  })
+                );
+
+                return (
+                  optionsPrices && (
+                    <Form.Item
+                      key={element.name}
+                      label={element.label}
+                      name={element.name}
+                      rules={element.rules}
+                      hasFeedback={element.hasFeedback}
+                      tooltip={element.tooltip}
+                      valuePropName={element.valuePropName}
+                    >
+                      {element.component(optionsPrices)}
+                    </Form.Item>
+                  )
+                );
+              }}
+            </Form.Item>
+          );
+        }
+
+        return (
+          <Form.Item
+            key={element.name}
+            label={element.label}
+            name={element.name}
+            rules={element.rules}
+            hasFeedback={element.hasFeedback}
+            tooltip={element.tooltip}
+            valuePropName={element.valuePropName}
+          >
+            {element.component}
+          </Form.Item>
+        );
+      })}
+    </Form>
   );
 };
 
 //Goods.propTypes = {}
 
 export default Goods;
-
-
