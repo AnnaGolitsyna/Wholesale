ae2172c2fff81ac5dc57a4f9f2fe233e1d63cbc0
AnnaGolitsyna <110576gav@gmail.com>
2024-01-04 13:51:31 +0200

    before

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -17,19 +13,10 @@ const firebaseConfig = {
 
 console.log('test', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
 
 //    apiKey: 'AIzaSyCppiIs_tP-UqFBdCSZMsaMExIiZnuwl0w',
 //     authDomain: 'wholesale2024-11201.firebaseapp.com',
