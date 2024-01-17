7388699c13d013518e3e61875c2df58841e11e5f
AnnaGolitsyna <110576gav@gmail.com>
2024-01-17 17:53:08 +0200

    refactor

--- src/features/catalog/utils/contractors/getContractorNameById.js
+++ src/features/catalog/utils/contractors/getContractorNameById.js
@@ -1,18 +1,7 @@
 const getContractorNameById = (itemId, list) =>
   list.find(({ id }) => id === itemId).name;
-  
 
-const getContractorLabelById = (itemId, list) => {
-  if (!itemId || !list) return;
-
-  const contractor = list.find(({ value }) => value === itemId);
-
-  if (!contractor) {
-    console.warn(`No contractor found for id ${itemId}`);
-    return;
-  }
-
-  return contractor.label;
-};
+const getContractorLabelById = (itemId, list) =>
+  list?.find(({ value }) => value === itemId).label;
 
 export { getContractorNameById, getContractorLabelById };
