b5dd92fc518d17e86ef70d1dafe68999aa1f7898
AnnaGolitsyna <110576gav@gmail.com>
2024-02-14 18:14:37 +0200

    refactor function getFieldsForFormList

--- src/components/formForModal/getFieldsForFormList.js
+++ src/components/formForModal/getFieldsForFormList.js
@@ -3,6 +3,16 @@ import { getFieldsForGoodsFormList } from '../../features/catalog/utils/goods/ge
 import { getFieldsForPaymentsFormList } from '../../features/finance/utils/getFormList';
 import { getAdditionalFieldsForContractorsFormList } from '../../features/catalog/utils/contractors/getAdditionalFormLists';
 
+/**
+ * Retrieves the fields for a form list based on the form type, action type, and data.
+ *
+ * @param {object} form - The form object
+ * @param {string} typeData - The type of data
+ * @param {string} actionType - The action type
+ * @param {object} data - The data object
+ * @return {type} The fields for the form list
+ */
+
 const getFieldsForFormList = (form, typeData, actionType, data) => {
   const typesObj = {
     Contractor: getFieldsForContractorsFormList,
@@ -11,11 +21,7 @@ const getFieldsForFormList = (form, typeData, actionType, data) => {
     Payment: getFieldsForPaymentsFormList,
   };
 
-  const getFormList = typesObj[typeData];
-
-  const formList = getFormList(form, actionType, data);
-
-  return formList;
+  return typesObj[typeData](form, actionType, data);
 };
 
 export { getFieldsForFormList };
