80ad9bd867d4e937c9dd059556a6709bb1d526cb
AnnaGolitsyna <110576gav@gmail.com>
2024-03-07 17:31:50 +0200

    deleted neddless in the ContractorPage

--- src/pages/Contractors/components/contractorsPage/ContractorsPage.jsx
+++ src/pages/Contractors/components/contractorsPage/ContractorsPage.jsx
@@ -1,8 +1,4 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectedContractorSelector } from '../../api/selectors';
-import { openModalContractor } from '../../api/contractorsSlice';
-import { formattedDateObj } from '../../../../utils/dateUtils';
 import { getContractorsColumns } from '../../utils/getColumns';
 import { getToolBarItems } from '../../utils/getToolBarItems';
 import { CatalogContent } from '../../../../modules/catalog';
@@ -10,11 +6,6 @@ import { useGetContractorsListQuery } from '../../api/contractorsApi';
 
 export const ContractorsPage = () => {
   const [activeStatus, setActiveStatus] = useState(true);
-  // const [actionType, setActionType] = useState(null);
-  // const { isContractorModalOpen, selectedContractor } = useSelector((state) =>
-  //   selectedContractorSelector(state)
-  // );
-  // const dispatch = useDispatch();
 
   const {
     data: contractorsList = [],
@@ -27,21 +18,6 @@ export const ContractorsPage = () => {
     const value = e.target.value === 'true' ? true : false;
     setActiveStatus(value);
   };
-
-  // const handleModifyContractor = (contractor, actionType) => {
-  //   const formattedContractor = contractor && {
-  //     ...contractor,
-  //     date: contractor.date ? formattedDateObj(contractor.date) : null,
-  //   };
-
-  //   setActionType(actionType);
-  //   dispatch(openModalContractor(formattedContractor));
-  // };
-
-  // const toolBarItems = getToolBarItems(
-  //   handleCheckboxChange,
-  //   handleModifyContractor
-  // );
 
   const columnsObject = getContractorsColumns(contractorsList);
 
@@ -58,7 +34,6 @@ export const ContractorsPage = () => {
         }}
         columnsObject={columnsObject}
         addToolBarItems={addToolBarItems}
-      
       />
     </>
   );
