ebe96b4b71073258d89e63e8a4cf08cf3fc9b37e
AnnaGolitsyna <110576gav@gmail.com>
2024-03-14 15:14:52 +0200

    complete style PrintPDF and TableToPrint

--- src/features/printingDocs/components/printComponent/PrintPDFComponent.jsx
+++ src/features/printingDocs/components/printComponent/PrintPDFComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 //import PropTypes from 'prop-types'
-import { Button } from 'antd';
+import { Button, Divider } from 'antd';
 import { ConfigProvider } from 'antd';
 import { useReactToPrint } from 'react-to-print';
 import TableToPrint from '../table/TableToPrint';
@@ -10,7 +10,6 @@ const PrintPDFComponent = ({ data, columns, title }) => {
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
-
     pageStyle: `@page { margin: 40px }`,
   });
 
@@ -19,15 +18,12 @@ const PrintPDFComponent = ({ data, columns, title }) => {
       <ConfigProvider
         theme={{
           inherit: false,
-          token: {
-            // colorBgBase: '#d9b08c',
-            // fontSize: 12,
-          },
         }}
       >
         <div style={{ margin: 10, padding: 10, border: '5px inset' }}>
           <div ref={componentRef}>
             <HeaderToPrint title={title} />
+            <Divider />
             <TableToPrint data={data} columns={columns} />
           </div>
         </div>
--- src/features/printingDocs/components/table/TableToPrint.jsx
+++ src/features/printingDocs/components/table/TableToPrint.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ConfigProvider } from 'antd';
 import { Table } from 'antd';
 
 const TableToPrint = ({ data, columns }) => {
   return (
     <>
-      {/* <ConfigProvider
-        theme={{
-          inherit: false,
-        }}
-      >
-        <Table
-          columns={columns}
-          dataSource={data}
-          pagination={false}
-          size="small"
-          bordered
-        />
-      </ConfigProvider> */}
       <Table
         columns={columns}
         dataSource={data}
