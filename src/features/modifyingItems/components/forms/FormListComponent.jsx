408f541aea0e79656a1161a49cb184614e676a26
AnnaGolitsyna <110576gav@gmail.com>
2024-04-10 15:40:28 +0300

    refactoring FormList

--- src/features/modifyingItems/components/forms/FormListComponent.jsx
+++ src/features/modifyingItems/components/forms/FormListComponent.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Space } from 'antd';
-import FormItemComponent from './FormItemComponent';
+import { Space } from 'antd';
+import { generateFormItem } from './generateFormItem';
 
 const FormListComponent = ({ data }) => {
   return (
     <>
-      {data?.map(({ keyname, children, ...itemProps }, index) => {
-        if (children) {
-
-          return (
-            <Space
-              key={keyname}
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'stretch',
-              }}
-            >
-              {children.map(({ keyname: childKey, ...childProps }, index) => {
-
-                return (
-                  <Form.Item
-                    key={childKey}
-                    {...childProps}
-                  >
-                    <FormItemComponent {...childProps} />
-                  </Form.Item>
-                );
-              })}
-            </Space>
-          );
-        } else {
-          return (
-            <Form.Item
-              key={keyname}
-              {...itemProps}
-            >
-              <FormItemComponent {...itemProps} />
-            </Form.Item>
-          );
-        }
-      })}
+      {data?.map(({ keyname, children, ...itemProps }) =>
+        !children ? (
+          generateFormItem({ keyname, ...itemProps })
+        ) : (
+          <Space
+            key={keyname}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'stretch',
+            }}
+          >
+            {children.map((childProps) => generateFormItem(childProps))}
+          </Space>
+        )
+      )}
     </>
   );
 };
