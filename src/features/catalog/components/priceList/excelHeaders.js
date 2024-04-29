c7386c97bf5a8f335f812cefd75c74ca82a7007d
AnnaGolitsyna <110576gav@gmail.com>
2024-04-29 17:23:26 +0300

    deleted old code components

--- src/features/catalog/components/priceList/excelHeaders.js
+++
@@ -1,8 +0,0 @@
-//  export const excelHeaders = [
-//   { label: 'Наименование', key: 'name' },
-//   { label: 'Поставщик', key: 'supplier' },
-//   { label: 'Закупка', key: 'cost' },
-//   { label: 'Кр.опт', key: 'superBulk' },
-//   { label: 'Опт', key: 'bulk' },
-//   { label: 'Розница', key: 'retail' },
-// ];
