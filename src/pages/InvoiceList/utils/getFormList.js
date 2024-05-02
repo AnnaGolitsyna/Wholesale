72b67c04dc476869cc6f107c12fa408e33f5d205
AnnaGolitsyna <110576gav@gmail.com>
2024-05-02 16:08:50 +0300

    create icone for Invoice

--- src/pages/InvoiceList/utils/getFormList.js
+++ src/pages/InvoiceList/utils/getFormList.js
@@ -1,4 +1,7 @@
-import { Typography } from 'antd';
+import { Typography, Input, InputNumber, DatePicker, Table } from 'antd';
+import {validateModifyingDate} from '../../../utils/dateUtils';
+import TreeSelectContractor from '../../../components/treeSelect/TreeSelectContractor';
+import FileIcon from '../../../styles/icons/FileIcon';
 
 const getFieldsForInvoiceFormList = (form, actionType, data) => {
   const titleText = {
@@ -9,10 +12,10 @@ const getFieldsForInvoiceFormList = (form, actionType, data) => {
     {
       keyname: 'title',
       children: [
-        // {
-        //   keyname: 'iconTitle',
-        //   component: <ClientIcon style={{ fontSize: 100 }} />,
-        // },
+        {
+          keyname: 'iconTitle',
+          component: <FileIcon />,
+        },
         {
           keyname: 'dynamicTitle',
           component: (
@@ -22,6 +25,109 @@ const getFieldsForInvoiceFormList = (form, actionType, data) => {
           ),
         },
       ],
+    },
+    {
+      keyname: 'transactionId',
+      children: [
+        {
+          name: 'docNumber',
+          keyname: 'docNumber',
+          label: 'Номер документа',
+          component: <Input disabled />,
+        },
+        {
+          keyname: 'date',
+          name: 'date',
+          label: 'Даты реализации',
+          component: <DatePicker placeholder="дата" format="YYYY-MM-DD" />,
+          rules: [
+            {
+              required: true,
+              message: 'Выберите дату',
+            },
+            () => ({
+              validator(_, value) {
+                return validateModifyingDate(data, value);
+              },
+            }),
+          ],
+        },
+      ],
+    },
+    {
+      keyname: 'name',
+      name: 'name',
+      label: 'Наименование контрагента',
+      hasFeedback: true,
+      rules: [{ required: true, message: 'Выберите поставщика из списка' }],
+      component: <TreeSelectContractor form={form} data={data} />,
+    },
+    {
+      keyname: 'type',
+      name: 'type',
+      label: 'Тип транзакции',
+      rules: [{ required: true, message: 'Выберите тип оплаты' }],
+      component: <Input />,
+      //   <RadioGroup form={form} data={data} />,
+    },
+    {
+      keyname: 'table',
+      name: 'productList',
+      label: 'Товары',
+      rules: [{ required: true, message: 'Выберите хотя бы один продукт' }],
+      component: (
+        <Table
+          dataSource={data?.productList}
+          columns={
+            [
+                 {
+                   title: 'Товар',
+                   dataIndex: 'name',
+                   key: 'name',
+                 },
+                 {
+                   title: 'Цена',
+                   dataIndex: 'price',
+                   key: 'price',
+                 },
+                 {
+                   title: 'Количество',
+                   dataIndex: 'count',
+                   key: 'count',
+                 },
+                 {
+                   title: 'Сумма',
+                   dataIndex: 'sum',
+                   key: 'sum',
+                  // render: (text) => text && getShortDateFormat(text),
+                 },
+            ]
+          }
+        />
+      ),
+    },
+
+    {
+      keyname: 'sum',
+      name: 'sum',
+      label: 'Сумма оплаты',
+      rules: [{ type: 'number', required: true, message: 'Заполните сумму' }],
+      component: (
+        <InputNumber
+          placeholder="сумма оплаты"
+          style={{
+            width: '100%',
+          }}
+          step={0.01}
+        />
+      ),
+    },
+
+    {
+      name: 'id',
+      keyname: 'id',
+      label: 'ID',
+      component: <Input disabled />,
     },
   ];
 };
---
+++ src/styles/icons/FileIcon.js
@@ -0,0 +1,225 @@
+import Icon from '@ant-design/icons';
+
+const FileSvg = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width="100"
+    height="100"
+    viewBox="0 0 64 64"
+  >
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHa_44004_gr1"
+      x1="41"
+      x2="41"
+      y1="25.5"
+      y2="36.571"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#94e5e7"></stop>
+      <stop offset="1" stopColor="#fff1e0"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHa_44004_gr1)"
+      d="M38,36h6c0.552,0,1-0.448,1-1v-8c0-0.552-0.448-1-1-1h-6c-0.552,0-1,0.448-1,1v8 C37,35.552,37.448,36,38,36z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHb_44004_gr2"
+      x1="32"
+      x2="32"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHb_44004_gr2)"
+      d="M49,10h-2c0-1.103-0.897-2-2-2h-8c0-1.103-0.897-2-2-2h-6c-1.103,0-2,0.897-2,2h-8 c-1.103,0-2,0.897-2,2h-2c-1.103,0-2,0.897-2,2v44c0,1.103,0.897,2,2,2h34c1.103,0,2-0.897,2-2V12C51,10.897,50.103,10,49,10z M29,8 h6v5c0,1.654-1.346,3-3,3s-3-1.346-3-3V8z M15,12h12v1c0,2.757,2.243,5,5,5s5-2.243,5-5v-1h12v32H39c-1.103,0-2,0.897-2,2v10H15V12z M39,56V46h10v10H39z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHc_44004_gr3"
+      x1="32"
+      x2="32"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHc_44004_gr3)"
+      d="M19 22H45V24H19z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHd_44004_gr4"
+      x1="26"
+      x2="26"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHd_44004_gr4)"
+      d="M19 26H33V28H19z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHe_44004_gr5"
+      x1="26"
+      x2="26"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHe_44004_gr5)"
+      d="M19 30H33V32H19z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHf_44004_gr6"
+      x1="26"
+      x2="26"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHf_44004_gr6)"
+      d="M19 34H33V36H19z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHg_44004_gr7"
+      x1="41"
+      x2="41"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHg_44004_gr7)"
+      d="M37 38H45V40H37z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHh_44004_gr8"
+      x1="20"
+      x2="20"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHh_44004_gr8)"
+      d="M19 48H21V52H19z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHi_44004_gr9"
+      x1="24"
+      x2="24"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHi_44004_gr9)"
+      d="M23 48H25V52H23z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHj_44004_gr10"
+      x1="28"
+      x2="28"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHj_44004_gr10)"
+      d="M27 48H29V52H27z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHk_44004_gr11"
+      x1="32"
+      x2="32"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHk_44004_gr11)"
+      d="M31 48H33V52H31z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHl_44004_gr12"
+      x1="43"
+      x2="43"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHl_44004_gr12)"
+      d="M41,50v2h2v-2h2v-2h-2C41.897,48,41,48.897,41,50z"
+    ></path>
+    <linearGradient
+      id="my3~0alKn8cYVsc_VJBgHm_44004_gr13"
+      x1="26"
+      x2="26"
+      y1="5.75"
+      y2="57.868"
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="reflect"
+    >
+      <stop offset="0" stopColor="#30c0c4"></stop>
+      <stop offset="1" stopColor="#ffcb9a"></stop>
+    </linearGradient>
+    <path
+      fill="url(#my3~0alKn8cYVsc_VJBgHm_44004_gr13)"
+      d="M19 38H33V40H19z"
+    ></path>
+  </svg>
+);
+
+const FileIcon = (props) => <Icon component={FileSvg} {...props} />;
+
+export default FileIcon;
