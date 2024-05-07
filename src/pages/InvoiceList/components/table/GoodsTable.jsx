020dd0817a43e174175eaa20d1ec3105d1d48797
AnnaGolitsyna <110576gav@gmail.com>
2024-05-07 18:24:51 +0300

    refactoring GoodsTable

--- src/pages/InvoiceList/components/table/GoodsTable.jsx
+++ src/pages/InvoiceList/components/table/GoodsTable.jsx
@@ -4,138 +4,56 @@ import {
   Space,
   Table,
   Spin,
-  Tag,
   Radio,
   ConfigProvider,
   theme,
   Typography,
-  Tooltip,
 } from 'antd';
 import { useGetGoodsListQuery } from '../../../Goods';
 import SearchInput from '../../../../components/searchInput/SearchInput';
-import { getFormattedDataForFilter } from '../../../../utils/getFormattedDataForFilter';
-import TagForNewDate from '../../../../components/tags/TagForNewDate';
+import { getColumns } from './getColumns';
 
 const GoodsTable = (props) => {
-  const { token } = theme.useToken();
   const { data, isLoading, isError } = useGetGoodsListQuery(true);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const [filteredList, setFilteredList] = useState(data);
-  // if (isLoading) return <Spin />;
-  //console.log('goodsTable', data, isLoading, isError);
-  const goodsList = data?.map(
-    ({ key, name, dateStart, cost, superBulk, bulk, retail, supplier }) => ({
-      key,
-      name,
-      dateStart,
-      supplier,
-      cost,
-      superBulk,
-      bulk,
-      retail,
-    })
-  );
-
-  const getFilteredGoodsList = (data) => {
-    if (!selectedRowKeys.length) {
-      setFilteredList(data);
-    } else {
-      const filteredData = data?.filter((item) =>
-        selectedRowKeys.includes(item.key)
-      );
-      setFilteredList(filteredData);
-    }
-    // return data?.filter((item) => selectedRowKeys.includes(item.key));
-  };
+  const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
-    getFilteredGoodsList(goodsList);
+    setFilteredList(data || []);
   }, [data]);
 
-  // const filteredGoodsList = goodsList?.filter((item) =>
-  //   selectedRowKeys.includes(item.key)
-  // );
-
-  console.log('filteredGoodsList', data, goodsList, filteredList);
-
-  const columns = [
-    {
-      title: 'Товар',
-      dataIndex: 'name',
-      key: 'name',
-      //   defaultSortOrder: 'ascend',
-      //   render: (name) => <>{name.label}</>,
-    },
-
-    {
-      title: 'В продаже',
-      dataIndex: 'dateStart',
-      key: 'dateStart',
-      render: (text) => <TagForNewDate date={text} color={token.tableAccent} />,
-    },
-    {
-      title: 'Закупка',
-      dataIndex: 'cost',
-      key: 'cost',
-    },
-    {
-      title: 'Кр.опт',
-      dataIndex: 'superBulk',
-      key: 'superBulk',
-    },
-    {
-      title: 'Опт',
-      dataIndex: 'bulk',
-      key: 'bulk',
-    },
-    {
-      title: 'Розница',
-      dataIndex: 'retail',
-      key: 'retail',
-    },
-    {
-      title: 'Контрагент',
-      dataIndex: 'supplier',
-      key: 'supplier',
-      // defaultSortOrder: 'ascend',
-      render: (supplier) => <>{supplier.label}</>,
-      filters: getFormattedDataForFilter(data),
-      onFilter: (value, record) => record.supplier.value === value,
-    },
-  ];
+  const { token } = theme.useToken();
   const onSelectChange = (newSelectedRowKeys) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
-    // setFilteredList(filteredGoodsList);
   };
+
   const rowSelection = {
     selectedRowKeys,
     onChange: onSelectChange,
   };
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    if (e.target.value === 'selected') {
-      const filteredData = goodsList?.filter((item) =>
-        selectedRowKeys.includes(item.key)
+  const handleChange = ({ target: { value } }) => {
+    if (value === 'selected') {
+      setFilteredList(
+        data?.filter((item) => selectedRowKeys.includes(item.key))
       );
-      setFilteredList(filteredData);
     } else {
-      setFilteredList(goodsList);
+      setFilteredList(data);
     }
   };
   const onSearch = (value) => {
-    console.log('search: ', value);
-    const foundItems = goodsList?.filter(({ name }) =>
+    const foundItems = data?.filter(({ name }) =>
       (name.label || name).toLowerCase().includes(value.toLowerCase())
     );
     setFilteredList(foundItems);
   };
+
+  const columns = getColumns(data, token);
+
   return (
     <>
       <Typography.Title level={3} style={{ textAlign: 'center' }}>
         Список товаров в реализации
       </Typography.Title>
-
       <Space
         style={{ margin: 10, display: 'flex', justifyContent: 'space-evenly' }}
       >
---
+++ src/pages/InvoiceList/components/table/getColumns.js
@@ -0,0 +1,48 @@
+import { getFormattedDataForFilter } from '../../../../utils/getFormattedDataForFilter';
+import TagForNewDate from '../../../../components/tags/TagForNewDate';
+
+export const getColumns = (data, token) => [
+  {
+    title: 'Товар',
+    dataIndex: 'name',
+    key: 'name',
+    //   defaultSortOrder: 'ascend',
+    //   render: (name) => <>{name.label}</>,
+  },
+
+  {
+    title: 'В продаже',
+    dataIndex: 'dateStart',
+    key: 'dateStart',
+    render: (text) => <TagForNewDate date={text} color={token.tableAccent} />,
+  },
+  {
+    title: 'Закупка',
+    dataIndex: 'cost',
+    key: 'cost',
+  },
+  {
+    title: 'Кр.опт',
+    dataIndex: 'superBulk',
+    key: 'superBulk',
+  },
+  {
+    title: 'Опт',
+    dataIndex: 'bulk',
+    key: 'bulk',
+  },
+  {
+    title: 'Розница',
+    dataIndex: 'retail',
+    key: 'retail',
+  },
+  {
+    title: 'Контрагент',
+    dataIndex: 'supplier',
+    key: 'supplier',
+    // defaultSortOrder: 'ascend',
+    render: (supplier) => <>{supplier.label}</>,
+    filters: getFormattedDataForFilter(data),
+    onFilter: (value, record) => record.supplier.value === value,
+  },
+];
