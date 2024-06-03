873a10188c295cb5264c761374015ddb17b27304
AnnaGolitsyna <110576gav@gmail.com>
2024-06-03 18:32:26 +0300

    RadioGroup is refactored

--- src/pages/InvoiceList/components/radioGroup/RadioGroupForInvoice.jsx
+++ src/pages/InvoiceList/components/radioGroup/RadioGroupForInvoice.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Radio, Form } from 'antd';
 import useInvoiceStyleByType from '../../hook/useInvoiceStyleByType';
 
 const RadioGroupForInvoice = () => {
   const form = Form.useFormInstance();
-
   const { modalDetails } = useInvoiceStyleByType();
 
-  const handleChange = (e) => {
-    form.setFieldsValue({ type: e.target.value });
+  const handleTypeChange = ({ target: { value } }) => {
+    form.setFieldsValue({ type: value });
   };
 
   return (
-    <>
-      <Radio.Group
-        buttonStyle="solid"
-        onChange={handleChange}
-        defaultValue={form.getFieldValue('type') || null}
-      >
-        <Radio.Button value="debet">
-          {modalDetails.debet.radioBtnText}
-        </Radio.Button>
-        <Radio.Button value="credit">
-          {modalDetails.credit.radioBtnText}
-        </Radio.Button>
-      </Radio.Group>
-    </>
+    <Radio.Group
+      buttonStyle="solid"
+      onChange={handleTypeChange}
+      defaultValue={form.getFieldValue('type') ?? null}
+    >
+      <Radio.Button value="debet">
+        {modalDetails.debet.radioBtnText}
+      </Radio.Button>
+      <Radio.Button value="credit">
+        {modalDetails.credit.radioBtnText}
+      </Radio.Button>
+    </Radio.Group>
   );
 };
 
-RadioGroupForInvoice.propTypes = {};
 
 export default RadioGroupForInvoice;
