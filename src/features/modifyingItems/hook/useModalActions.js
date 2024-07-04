1ef1b2cb64c7e25b5efb1da381dc8f6dcb198801
AnnaGolitsyna <110576gav@gmail.com>
2024-07-04 17:48:21 +0300

    useModalAction is refactored

--- src/features/modifyingItems/hook/useModalActions.js
+++ src/features/modifyingItems/hook/useModalActions.js
@@ -1,26 +1,27 @@
 import {
   useAddGoodsMutation,
   useUpdateProductMutation,
+  getFieldsForGoodsFormList,
 } from '../../../pages/Goods';
 import {
   useAddContractorMutation,
   useUpdateContractorMutation,
+  getFieldsForContractorsFormList,
+  getAdditionalFieldsForContractorsFormList,
 } from '../../../pages/Contractors';
-import { createPayment, updatePayment } from '../../../pages/Payments';
-import { createInvoice, updateInvoice } from '../../../pages/InvoiceList';
-import { FORM_TYPES } from '../constant/formTypes';
-
-import { getFieldsForContractorsFormList } from '../../../pages/Contractors';
-import { getAdditionalFieldsForContractorsFormList } from '../../../pages/Contractors';
-import { getFieldsForGoodsFormList } from '../../../pages/Goods';
-import { getFieldsForPaymentsFormList } from '../../../pages/Payments';
 import {
+  createPayment,
+  updatePayment,
+  getFieldsForPaymentsFormList,
+} from '../../../pages/Payments';
+import {
+  createInvoice,
+  updateInvoice,
   getFieldsForInvoiceFormList,
   getAdditionalFieldsForInvoiceFormList,
   getEmptyFieldFormList,
 } from '../../../pages/InvoiceList';
-
-//console.log('test', getFieldsForContractorsFormList);
+import { FORM_TYPES } from '../constant/formTypes';
 
 /**
  * Returns the action list for the given type data.
@@ -28,58 +29,14 @@ import {
  * @param {string} typeData - The type of data to retrieve action list for
  * @return {Object} The action list for the given type data
  */
-// const useModalActions = (typeData) => {
-//   const [addContractor] = useAddContractorMutation();
-//   const [updateContractor] = useUpdateContractorMutation();
-//   const [addGoods] = useAddGoodsMutation();
-//   const [updateProduct] = useUpdateProductMutation();
-
-//   const actionList = {
-//     Contractor: {
-//       createItem: addContractor,
-//       updateItem: updateContractor,
-//       btnText: 'Создать нового контрагента',
-//     },
-//     ContractorAdditional: {
-//       // createItem: addContractor,
-//       // updateItem: updateContractor,
-//       btnText: 'Добавить связанную компанию - посредника',
-//     },
-//     Goods: {
-//       createItem: addGoods,
-//       updateItem: updateProduct,
-//       btnText: 'Создать новый товар',
-//     },
-//     Payment: {
-//       createItem: createPayment,
-//       updateItem: updatePayment,
-//       btnText: 'Создать новую оплату',
-//     },
-//     Invoice: {
-//       createItem: createInvoice,
-//       updateItem: updateInvoice,
-//       btnText: 'Создать новый документ',
-//     },
-
-//     InvoiceProductsAdditional: {
-//       btnText: 'Выбрать товары',
-//     },
-//     InvoiceEmptyAdditional: {
-//       btnText: 'Ввести вручную',
-//     },
-//   };
-
-//   return actionList[typeData];
-// };
 
 const useModalActions = (typeData) => {
-  // Call all hooks at the top level
+
   const [addContractor] = useAddContractorMutation();
   const [updateContractor] = useUpdateContractorMutation();
   const [addGoods] = useAddGoodsMutation();
   const [updateProduct] = useUpdateProductMutation();
 
-  // Use switch only to return the appropriate object
   switch (typeData) {
     case FORM_TYPES.CONTRACTOR:
       return {
@@ -90,8 +47,6 @@ const useModalActions = (typeData) => {
       };
     case FORM_TYPES.CONTRACTOR_ADDITIONAL:
       return {
-        // createItem: addContractor,
-        // updateItem: updateContractor,
         getFields: getAdditionalFieldsForContractorsFormList,
         btnText: 'Добавить связанную компанию - посредника',
       };
