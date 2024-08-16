db220107c2ef98bd8bca40063a4b6e351363f52c
AnnaGolitsyna <110576gav@gmail.com>
2024-08-16 14:13:28 +0300

    create short format for contractor names

--- src/pages/ContractorReceivable/api/operations.js
+++ src/pages/ContractorReceivable/api/operations.js
@@ -13,7 +13,7 @@ import { getTransactionsListByIdRef } from './firebaseRefs';
 
 const getContractorReceivableData = async (id) => {
   try {
-    const receivableDocRef = await getReceivableDocRef(id);
+    const receivableDocRef = getReceivableDocRef(id);
     const receivableDocSnap = await getDoc(receivableDocRef);
 
     if (receivableDocSnap.exists()) {
@@ -34,14 +34,12 @@ const getContractorReceivableData = async (id) => {
   }
 };
 
-// getTransactionsDataByIdAndRange 
+// getTransactionsDataByIdAndRange
 // input: period,id
 // use getMonthsInRange(period)
 // Promise.all
 // maping months and fetching data by month, id
 // output: transactions[]
-
-
 
 // getTransactionsDataByIdAndMonth
 
@@ -49,16 +47,8 @@ const getTransactionsDataById = async (id) => {
   const results = [];
 
   try {
-    const invoicesQuery = getTransactionsListByIdRef(
-      'invoices',
-      '2024-07',
-      id
-    );
-    const paymentsQuery = getTransactionsListByIdRef(
-      'payments',
-      '2024-07',
-      id
-    );
+    const invoicesQuery = getTransactionsListByIdRef('invoices', '2024-07', id);
+    const paymentsQuery = getTransactionsListByIdRef('payments', '2024-07', id);
 
     const [invoicesSnapshot, paymentsSnapshot] = await Promise.all([
       getDocs(invoicesQuery),
@@ -91,52 +81,3 @@ const getTransactionsDataById = async (id) => {
 };
 
 export { getContractorReceivableData, getTransactionsDataById };
-
-// const getTransactionsDataById = async (id) => {
-//   const results = [];
-
-//   try {
-//     console.log('Querying with id:', id);
-
-//     const invoicesRef = collection(...getRef('invoices', '2024-07-07'));
-//     const invoicesQuery = query(invoicesRef, where('name.value', '==', id));
-//     console.log('Invoices query:', invoicesQuery);
-
-//     const invoicesSnapshot = await getDocs(invoicesQuery);
-//     console.log('Invoices snapshot size:', invoicesSnapshot.size);
-
-//     const paymentsRef = collection(...getRef('payments', '2024-07-07'));
-//     const paymentsQuery = query(paymentsRef, where('name.value', '==', id));
-//     console.log('Payments query:', paymentsQuery);
-
-//     const paymentsSnapshot = await getDocs(paymentsQuery);
-//     console.log('Payments snapshot size:', paymentsSnapshot.size);
-
-//     invoicesSnapshot.forEach((doc) => {
-//       console.log('Invoice doc:', doc.id, doc.data());
-//       results.push({ ...doc.data(), id: doc.id });
-//     });
-
-//     paymentsSnapshot.forEach((doc) => {
-//       console.log('Payment doc:', doc.id, doc.data());
-//       const newData = {
-//         ...doc.data(),
-//         id: doc.id,
-//         docType: 'payments',
-//       };
-//       results.push(newData);
-//     });
-
-//     console.log('Final results:', results);
-
-//     return results.map((el) => {
-//       return {
-//         ...el,
-//         label: operationTypes[el.docType][el.type].text,
-//       };
-//     });
-//   } catch (error) {
-//     console.error('Error fetching transaction data:', error);
-//     throw error;
-//   }
-// };
--- src/pages/Dashboard/components/charts/receivableChart/CustomizedAxisTick.jsx
+++ src/pages/Dashboard/components/charts/receivableChart/CustomizedAxisTick.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-  const CustomizedAxisTick = ({ color, ...props }) => {
-    const { x, y, payload } = props;
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <text
-          x={0}
-          y={0}
-          dy={16}
-          textAnchor="end"
-          fill={color}
-          transform="rotate(-20)"
-        >
-          {payload.value}
-        </text>
-      </g>
-    );
-  };
+const CustomizedAxisTick = ({ color, ...props }) => {
+  const { x, y, payload } = props;
+  const shortName = `${payload.value.slice(0, 5)}...`;
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        textAnchor="end"
+        fill={color}
+        transform="rotate(-90)"
+      >
+        {shortName}
+      </text>
+    </g>
+  );
+};
 
 CustomizedAxisTick.propTypes = {
   color: PropTypes.string,
   payload: PropTypes.object,
   x: PropTypes.number,
   y: PropTypes.number,
-}
+};
 
-export default CustomizedAxisTick
+export default CustomizedAxisTick;
