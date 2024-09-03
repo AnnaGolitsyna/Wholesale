07f3ee8022907a378115a325ac10df0b48b0f69b
AnnaGolitsyna <110576gav@gmail.com>
2024-09-03 13:08:41 +0300

    create ReceivableHeader

---
+++ src/pages/Receivable/components/header/ReceivableHeader.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Typography } from 'antd';
+import SearchInput from '../../../../components/searchInput/SearchInput';
+import { getTodayFullFormattedDate } from '../../../../utils/dateUtils';
+import { boxStyle } from '../../../../styles/boxStyle';
+
+const { Title } = Typography;
+
+const ReceivableHeader = ({onChange}) => {
+  return (
+    <Row>
+      <Col span={16}>
+        <Title
+          level={3}
+          style={{ textAlign: 'center', margin: '0 0 20px 0', ...boxStyle }}
+        >
+          {`Дебиторская задолженность на ${getTodayFullFormattedDate()}`}
+        </Title>
+      </Col>
+      <Col span={1}></Col>
+      <Col span={7}>
+        <SearchInput onChange={onChange} placeholder="Поиск по договору" />
+      </Col>
+    </Row>
+  );
+};
+
+ReceivableHeader.propTypes = {
+    onChange: PropTypes.func.isRequired,
+};
+
+export default ReceivableHeader;
--- src/pages/Receivable/components/layout/ReceivableLayout.jsx
+++ src/pages/Receivable/components/layout/ReceivableLayout.jsx
@@ -4,6 +4,7 @@ import { Row, Col, Typography, Space, Card, Divider } from 'antd';
 import { getTodayFullFormattedDate } from '../../../../utils/dateUtils';
 import SearchInput from '../../../../components/searchInput/SearchInput';
 import { boxStyle } from '../../../../styles/boxStyle';
+import ReceivableHeader from '../header/ReceivableHeader';
 
 const { Title, Text } = Typography;
 
@@ -20,23 +21,7 @@ const ReceivableLayout = ({ renderList }) => {
 
   return (
     <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <Row>
-        <Col span={16}>
-          <Title
-            level={3}
-            style={{ textAlign: 'center', margin: '0 0 20px 0', ...boxStyle }}
-          >
-            {`Дебиторская задолженность на ${getTodayFullFormattedDate()}`}
-          </Title>
-        </Col>
-        <Col span={1}></Col>
-        <Col span={7}>
-          <SearchInput
-            onChange={handleChange}
-            placeholder="Поиск по договору"
-          />
-        </Col>
-      </Row>
+      <ReceivableHeader onChange={handleChange} />
 
       <Row gutter={[16, 16]} justify="space-around" style={{ flex: 1 }}>
         {renderList.map(({ icon, title, component }, index) => (
